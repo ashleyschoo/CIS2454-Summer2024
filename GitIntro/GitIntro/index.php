b025dfbd33460722ae29d7fdc2f395afc2419cae
ashleyschoo <30761294+ashleyschoo@users.noreply.github.com>
2024-05-21 17:59:51 -0400

    creating index file

---
+++ GitIntro/GitIntro/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php 
+        echo "<h2>hello world</h2>";
+        echo "
+            <h3>
+            1 - Name, and major / reason for taking the class
+                Chris Hutchons, software engineering associates degree
+            2 - Any previous programming experience that they have
+                Java, Javascript, HTML5, CSS
+            3 - Do they have any hobbies?
+                Magic the Gathering, skateboarding, video games
+            4 - Do they have any plans for the Fall aside from school?
+                more haunted houses and cider mills!
+            5 - Anything else they'd like the class to know about them.
+                I'm a big fan of movie theaters and people should get out and watch movies!
+            </h3>";
+        ?>
+    </body>
+</html>
