3529759d6f53e3478c1d11820f82c0a7ada9d64d
ashleyschoo <30761294+ashleyschoo@users.noreply.github.com>
2024-05-21 21:55:32 -0400

    Update index.php

--- GitIntro/GitIntro/index.php
+++ GitIntro/GitIntro/index.php
@@ -7,19 +7,20 @@
     </head>
     <body>
         <?php 
-        echo "<h2>hello world</h2>";
+        echo "<h2>Chris Hutchons Introduction</h2>";
         echo "
             <h3>
-            1 - Name, and major / reason for taking the class
-                Chris Hutchons, software engineering associates degree
-            2 - Any previous programming experience that they have
-                Java, Javascript, HTML5, CSS
-            3 - Do they have any hobbies?
-                Magic the Gathering, skateboarding, video games
-            4 - Do they have any plans for the Fall aside from school?
-                more haunted houses and cider mills!
-            5 - Anything else they'd like the class to know about them.
-                I'm a big fan of movie theaters and people should get out and watch movies!
+            <br>
+            1 - Name, and major / reason for taking the class <br>
+                Chris Hutchons, software engineering associates degree <br>
+            2 - Any previous programming experience that they have <br>
+                Java, Javascript, HTML5, CSS <br>
+            3 - Do they have any hobbies? <br>
+                Magic the Gathering, skateboarding, video games <br>
+            4 - Do they have any plans for the Fall aside from school? <br>
+                more haunted houses and cider mills! <br>
+            5 - Anything else they'd like the class to know about them. <br>
+                I'm a big fan of movie theaters and people should get out and watch movies! <br>
             </h3>";
         ?>
     </body>
